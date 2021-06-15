70a4ddad266530e5bdd85c997c7e2abd58069035
SpareSimian <SpareSimian+github@SewingWitch.com>
2021-06-15 01:52:39 -0700

    Wait for message to be sent by flushing queue and sleeping (only on shutdown).

--- bootmail.sh
+++ bootmail.sh
@@ -2,6 +2,9 @@
 
 # https://askubuntu.com/questions/851946/send-me-an-email-on-computer-shutdown
 
+# exit with failure if any commands fail
+set -e
+
 EMAIL="root"
 
 if [ "$1" = startup ]
@@ -16,3 +19,12 @@ fi
 SUBJECT="[$HOSTNAME] - System $1"
 
 echo "$BODY" | mail -S sendwait -s "${SUBJECT}" "${EMAIL}"
+
+# flush the queue to push mail to remote servers
+/usr/sbin/sendmail -q
+
+# for shutdown, give some time for remote connection to complete
+if [ "$1" != startup ]
+then
+    sleep 5
+fi
